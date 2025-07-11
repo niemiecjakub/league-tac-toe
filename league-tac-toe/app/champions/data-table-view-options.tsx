48eb14279cf4328d7344d22d51083f7ba33e8775
niemiecjakub <kniemiec3@gmail.com>
2025-07-11 19:48:38 +0200

    Champion table with filtering

--- league-tac-toe/app/champions/data-table-view-options.tsx
+++ league-tac-toe/app/champions/data-table-view-options.tsx
@@ -3,7 +3,6 @@
 import { DropdownMenuTrigger } from "@radix-ui/react-dropdown-menu";
 import { Table } from "@tanstack/react-table";
 import { Settings2 } from "lucide-react";
-
 import { Button } from "@/components/ui//button";
 import { DropdownMenu, DropdownMenuCheckboxItem, DropdownMenuContent, DropdownMenuLabel, DropdownMenuSeparator } from "@/components/ui//dropdown-menu";
 
--- league-tac-toe/services/championService.ts
+++ league-tac-toe/services/championService.ts
@@ -1,7 +1,13 @@
 import axios from "@/lib/axios";
 import { Champion } from "@/models/Champion";
+import { ChampionMetaFilter } from "@/models/MetaItem";
 
 export const getChampions = async (): Promise<Champion[]> => {
     const { data } = await axios.get("/Champion/all");
+    return data;
+};
+
+export const getMetaItems = async (): Promise<ChampionMetaFilter> => {
+    const { data } = await axios.get("/Meta");
     return data;
 };
