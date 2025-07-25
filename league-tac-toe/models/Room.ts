e8991a78958716b55c418767b04e708ed1ad8053
niemiecjakub <kniemiec3@gmail.com>
2025-07-25 22:33:54 +0200

    Handle finding random opponent

--- league-tac-toe/models/Room.ts
+++ league-tac-toe/models/Room.ts
@@ -2,9 +2,11 @@ export interface Room {
     roomGuid: string;
     turnTime: number | null;
     stealsEnabled: boolean;
+    isPublic: boolean;
 }
 
 export interface RoomOptions {
     turnTime: number | null;
     stealsEnabled: boolean;
+    isPublic: boolean;
 }
