e48a6589cf9ea6967a2f1bf8e8da06c29cf6d6d9
niemiecjakub <kniemiec3@gmail.com>
2025-10-12 09:58:14 +0200

    base internaliaztion

--- league-tac-toe/app/layout.tsx
+++ league-tac-toe/app/layout.tsx
@@ -1,24 +1,23 @@
 import type { Metadata } from "next";
 import "./globals.css";
 import Navbar from "@/components/custom/navbar";
+import { NextIntlClientProvider } from "next-intl";
 
 export const metadata: Metadata = {
     title: "League Tac Toe",
     description: "A game of Tic Tac Toe with League of Legends champions.",
 };
 
-export default function RootLayout({
-    children,
-}: Readonly<{
-    children: React.ReactNode;
-}>) {
+export default async function RootLayout({ children }: Readonly<{ children: React.ReactNode }>) {
     return (
         <html lang="en">
             <body className="antialiased">
-                <div className="w-full max-h-screen flex flex-col items-center">
-                    <Navbar />
-                    <div className="flex h-screen flex-col items-center w-full px-2 sm:px-0">{children}</div>
-                </div>
+                <NextIntlClientProvider>
+                    <div className="w-full max-h-screen flex flex-col items-center">
+                        <Navbar />
+                        <div className="flex h-screen flex-col items-center w-full px-2 sm:px-0">{children}</div>
+                    </div>
+                </NextIntlClientProvider>
             </body>
         </html>
     );
