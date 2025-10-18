af11d531f48ff1418ec9dc75c37e70b97da5ef41
niemiecjakub <kniemiec3@gmail.com>
2025-10-18 21:01:13 +0200

    Dashboard styling

--- league-tac-toe/components/custom/timer.tsx
+++ league-tac-toe/components/custom/timer.tsx
@@ -1,9 +1,10 @@
 "use client";
+
 import { useTranslations } from "next-intl";
 import { useRoomStore } from "@/store/roomStore";
 
 export default function Timer() {
     const t = useTranslations("game.dashboard");
     const { room, turnTimeLeft } = useRoomStore((state) => state);
-    return <>{room?.turnTime == null ? <p>{t("noTimeLimit")}</p> : <p className="text-sm text-gray-500">{turnTimeLeft}</p>}</>;
+    return room?.turnTime == null ? <span className="text-sm text-gray-600">{t("noTimeLimit")}</span> : <span className="text-lg font-semibold">00:{String(turnTimeLeft).padStart(2, "0")}</span>;
 }
