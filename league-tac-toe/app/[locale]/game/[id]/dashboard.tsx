0dc727e2dcc1617805d2dd3ac14f18ebff1e6a36
niemiecjakub <kniemiec3@gmail.com>
2025-10-18 21:01:13 +0200

    Dashboard styling

--- league-tac-toe/app/[locale]/game/[id]/dashboard.tsx
+++ league-tac-toe/app/[locale]/game/[id]/dashboard.tsx
@@ -30,20 +30,20 @@ export default function Dashboard() {
         }
 
         return (
-            <Button variant={isYourTurn() ? `default` : `secondary`} onClick={handleSendDrawRequest}>
-                {t("draw.requestDraw")}
+            <Button variant={isYourTurn() ? "default" : "ghost"} onClick={handleSendDrawRequest} className="px-1 py-0">
+                {isYourTurn() && t("draw.requestDraw")}
             </Button>
         );
     }
 
     return (
         <div className="flex flex-col items-center w-full py-2">
-            <div className=" w-full flex justify-start">
-                <p className="text-sm ">{t("youAre", { player: PlayerType[room?.slot.playerType!] })}</p>
-            </div>
             <div className="w-full flex justify-between items-center">
-                <div className="flex flex-col">
-                    <div className="flex w-full text-lg font-semibold">
+                <div className="flex flex-col items-center justify-center">
+                    <div className=" w-full flex justify-start">
+                        <p className="text-sm ">{t("youAre", { player: PlayerType[room?.slot.playerType!] })}</p>
+                    </div>
+                    <div className="flex w-full font-semibold text-xl">
                         <div>{t("you")}</div>
                         <div className="px-2">
                             {room?.score.you} - {room?.score.opponent}
@@ -52,13 +52,15 @@ export default function Dashboard() {
                     </div>
                 </div>
                 <div className="flex space-x-2 items-center">
-                    <p className="text-lg font-semibold">{isYourTurn() ? `${t("yourTurn")}` : `${t("opponentsTurn")}`}</p>
-                    <Button variant={isYourTurn() ? `destructive` : `secondary`} onClick={handleTurnSkip}>
-                        {t("skipTurn")}
-                    </Button>
+                    <div className="flex items-center justify-center bg-gray-300 rounded-lg text-sm h-[36px]">
+                        <p className="px-2">{isYourTurn() ? `${t("yourTurn")}` : `${t("opponentsTurn")}`}</p>
+                        <Button variant="destructive" onClick={handleTurnSkip} className={`px-1 py-0 ${isYourTurn() ? "" : "hidden"}`}>
+                            {t("skipTurn")}
+                        </Button>
+                    </div>
                 </div>
             </div>
-            <div className="w-full flex justify-between items-center">
+            <div className="w-full flex justify-between items-center h-[36px]">
                 <Timer />
                 <div>{getDrawControls()}</div>
             </div>
