000ea2e70d68d6fc74c900a95490dba0235c10dd
niemiecjakub <kniemiec3@gmail.com>
2025-10-20 21:39:54 +0200

    Docker files

--- league-tac-toe/next.config.ts
+++ league-tac-toe/next.config.ts
@@ -3,6 +3,14 @@ import createNextIntlPlugin from "next-intl/plugin";
 
 const nextConfig: NextConfig = {
     reactStrictMode: false,
+    images: {
+        remotePatterns: [
+            {
+                protocol: "https",
+                hostname: "ddragon.leagueoflegends.com",
+            },
+        ],
+    },
 };
 
 const withNextIntl = createNextIntlPlugin();
