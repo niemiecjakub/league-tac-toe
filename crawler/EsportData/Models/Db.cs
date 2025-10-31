d6385a848eaeb58cecca526400445d371e15d5e3
niemiecjakub <kniemiec3@gmail.com>
2025-10-31 16:55:21 +0100

    Delete sepearete crawler proj

--- crawler/EsportData/Models/Db.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace EsportData.Models
-{
-  public record League(int Id, string Name);
-  public record Game(string Id, string LeagueId, DateTime? Date, string Patch, int Year);
-  public record Player(string Name);
-  public record Team(string Name);
-  public record GameBan(string GameId, string TeamId, string BannedChampionId);
-  public record PlayerChampion(string PlayerId, string ChampionId, string GameId, string TeamId, PlayerPosition? Position, GameResult GameResult);
-}
