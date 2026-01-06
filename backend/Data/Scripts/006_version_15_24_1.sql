c4dcbdde8b78945011afb5bc609960903b175c18
niemiecjakub <kniemiec3@gmail.com>
2026-01-06 22:03:56 +0100

    Version 15.24.1 - Zaahen added

---
+++ backend/Data/Scripts/006_version_15_24_1.sql
@@ -0,0 +1,13 @@
+INSERT INTO "Champion" VALUES(172, 'zaahen', 'Zaahen', 'the Unsundered', 'https://ddragon.leagueoflegends.com/cdn/15.24.1/img/champion/Zaahen.png', '2025-11-19');
+
+INSERT INTO "ChampionLegacy" VALUES(172,2);
+INSERT INTO "ChampionLegacy" VALUES(172,4);
+
+INSERT INTO "ChampionPosition" VALUES(172,1);
+INSERT INTO "ChampionPosition" VALUES(172,3);
+
+INSERT INTO "ChampionRangeType" VALUES(172,1);
+
+INSERT INTO "ChampionRegion" VALUES(172,13);
+
+INSERT INTO "ChampionResource" VALUES(172,1);
